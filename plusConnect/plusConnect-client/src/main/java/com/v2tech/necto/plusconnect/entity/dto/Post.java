fc316796f60ea8a36957c420ed6ebf74888d50ef
puneetsharma <sharmapuneet1510@gmail.com>
2015-10-10 14:35:27 +0530

    Post Class Commit

--- plusConnect/plusConnect-client/src/main/java/com/v2tech/necto/plusconnect/entity/dto/Post.java
+++ plusConnect/plusConnect-client/src/main/java/com/v2tech/necto/plusconnect/entity/dto/Post.java
@@ -21,4 +21,68 @@ public class Post implements Serializable
 		private User postedBy;
 		private Date postedOn;
 		private String message;
+
+		public PostType getPostType()
+			{
+				return postType;
+			}
+
+		public void setPostType(PostType postType)
+			{
+				this.postType = postType;
+			}
+
+		public User getPostedBy()
+			{
+				return postedBy;
+			}
+
+		public void setPostedBy(User postedBy)
+			{
+				this.postedBy = postedBy;
+			}
+
+		public Date getPostedOn()
+			{
+				return postedOn;
+			}
+
+		public void setPostedOn(Date postedOn)
+			{
+				this.postedOn = postedOn;
+			}
+
+		public String getMessage()
+			{
+				return message;
+			}
+
+		public void setMessage(String message)
+			{
+				this.message = message;
+			}
+
+		/**
+		 * <br>
+		 * @Author : puneetsharma <br>
+		 * @createdDate : 10-Oct-2015 <br>
+		 * @createdTime : 2:32:02 pm <br>
+		 * @methodPackage : com.v2tech.necto.plusconnect.entity.dto <br>
+		 * @Description : <br>
+		 * @StepsWithExplanation : <br>
+		 * @TODO : <br>
+		 * @param postType <br>
+		 * @param postedBy <br>
+		 * @param postedOn <br>
+		 * @param message
+		 */
+		public Post(PostType postType, User postedBy, Date postedOn, String message)
+			{
+				super();
+				this.postType = postType;
+				this.postedBy = postedBy;
+				this.postedOn = postedOn;
+				this.message = message;
+			}
+
 	}
